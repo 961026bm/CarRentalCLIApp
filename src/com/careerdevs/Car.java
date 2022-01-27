2351f19fcbd08b4b09316cb56a366c38a3b0fddb
961026bm <olubunmiosunde.student@careerdevs.com>
2022-01-27 18:27:49 -0500

    cli committed

---
+++ src/com/careerdevs/Car.java
@@ -0,0 +1,33 @@
+package com.careerdevs;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Car {
+    private String make;
+    private String model;
+    private boolean isRented;
+
+ public Car(String make, String model) {
+        this.make = make;
+        this.model = model;
+        this.isRented = false;
+    }
+    // this method is to access the value of make
+    public String getMake() {
+        return make;
+    }
+    // this method is to access the value of model
+    public String getModel() {
+        return model;
+    }
+
+    public String getName(){
+        return make + " " + model;
+    }
+
+    // this method is to access the value of isRented
+    public void setRented(boolean rented) {
+        isRented = rented;
+    }
+
+}
---
+++ src/com/careerdevs/RentalService.java
@@ -0,0 +1,44 @@
+package com.careerdevs;
+
+import java.util.Scanner;
+
+public class RentalService {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Welcome to BM Car Rentals App");
+//        System.out.println("please enter a number to select the car you will like to rent./n 1 2 or 3: ");
+//        String user = scanner.nextLine();
+//        System.out.println("you have chosen car" + user);
+//
+//        System.out.println("Thank you, your order will be processed, pls enter your name");
+//        String user1 = scanner.nextLine();
+//        System.out.println(user1 + " How many days do you need the car?");
+//        short user2 = scanner.nextShort();
+//        System.out.println("You have selected " + user2 + " days");
+
+
+
+        Car[] carStorage = new Car[3];
+
+        // create obj instances
+        Car car1 = new Car("Honda", "Accord");
+        Car car2 = new Car("Chevy", "Cruze");
+        Car car3 = new Car("Toyota", "Corolla");
+
+        // store obj instances in list/arr
+        carStorage[0] = car1;
+        carStorage[1] = car2;
+        carStorage[2] = car3;
+
+        for (int i = 0; i < carStorage.length; i++) {
+            //System.out.println(car1.getMake()); // this will output Honda
+            System.out.println(carStorage[i].getMake()); // this output honda, chevy & toyota
+
+        }
+
+
+
+    }
+}
